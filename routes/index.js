4c8c1bd0aee35460c99e04413fbb3e026ef629c8
Lee Myring <mail@thinkstack.io>
2018-08-29 16:50:32 +0100

    Added version to webpage

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var version = fs.readFileSync('../.version', 'utf8');
+var version = fs.readFileSync('.version', 'utf8');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
